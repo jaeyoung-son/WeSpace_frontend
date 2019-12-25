5878f0f541622b49082ad750d491a69422ed818a
nabisorry <chdb57@naver.com>
2019-12-25 21:29:00 +0900

    메인 슬라이드 이미지 구현

---
+++ src/Components/SlideItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./SlideItem.scss";
+import { Link } from "react-router-dom";
+
+class SlideItem extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <Link to="/" className="slide_wrap">
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${this.props.slideImage})` }} className="slideItem_image">
+            <div className="slideItem_article">
+              <h3 className="slideItem_title">제목</h3>
+              <p className="slideItem_text">
+                워크샵 장소를 <br />
+                추천해 드립니다
+              </p>
+              <br />
+              <div className="slideItem_btn">바로가기</div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    );
+  }
+}
+
+export default SlideItem;
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from "react";
+import MainSlideImages from "../../Components/MainSlideImages";
 
 const Main = () => {
-  return (
-    <div>
-      메인페이지
-    </div>
-  )
-}
+  return <MainSlideImages />;
+};
 
-export default Main
+export default Main;
