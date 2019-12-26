8537e49e1c0cf402f60ac15095dcbc49791b0dfc
nabisorry <chdb57@naver.com>
2019-12-26 21:25:57 +0900

    main.js 충돌 수정 2차

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,15 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import MainSlideImages from "../../Components/MainSlideImages";
+import MainSlideImages from "../../Components/MainGnb";
 
 const Main = () => {
   return <MainSlideImages />;
-=======
-import MainGnb from "../../Components/MainGnb";
-
-const Main = () => {
-  return <MainGnb />;
->>>>>>> 4da3f66735756b11ba58262520e9f2806057751d
 };
 
 export default Main;
