cda6df3a773a4ea0ef83e0b055fa63160f1e09d1
nabisorry <chdb57@naver.com>
2019-12-30 23:30:43 +0900

    카카오 지도 api 구현

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MainSlideImages from "../../Components/MainSlideImages";
 import MainGnb from "../../Components/MainGnb";
+import KaKaoMap from "../../Components/KaKaoMap";
 
 const Main = () => {
   return (
     <>
       <MainGnb />
       <MainSlideImages />
+      <KaKaoMap address={"도영로 80"} />
     </>
   );
 };
